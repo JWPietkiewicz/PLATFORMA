2cdb2cf5bf7cf0ea7a6b654386ab3150df249805
JWPietkiewicz <50460041+JWPietkiewicz@users.noreply.github.com>
2025-08-06 14:51:17 +0200

    feat: add theme switcher with Fluent provider

--- platforma/src/App.jsx
+++ platforma/src/App.jsx
@@ -1,4 +1,6 @@
 import { initializeIcons, Stack } from '@fluentui/react';
+import { FluentProvider, webLightTheme, webDarkTheme } from '@fluentui/react-components';
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import ImageCarousel from './components/ImageCarousel';
 import Footer from './components/Footer';
@@ -6,12 +8,16 @@ import Footer from './components/Footer';
 initializeIcons();
 
 export default function App() {
+  const [isDark, setIsDark] = useState(false);
+
   return (
-    <Stack tokens={{ childrenGap: 20 }}>
-      <Navbar />
-      <ImageCarousel />
-      <Footer />
-    </Stack>
+    <FluentProvider theme={isDark ? webDarkTheme : webLightTheme}>
+      <Stack tokens={{ childrenGap: 20 }}>
+        <Navbar isDark={isDark} setIsDark={setIsDark} />
+        <ImageCarousel />
+        <Footer />
+      </Stack>
+    </FluentProvider>
   );
 }
 
