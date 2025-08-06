95695bb6c38d14c7ebccb57574c1c2b5abbaed73
JWPietkiewicz <50460041+JWPietkiewicz@users.noreply.github.com>
2025-08-06 17:34:22 +0200

    demo: showcase light and dark article themes

--- platforma/src/components/ArticleExample.tsx
+++ platforma/src/components/ArticleExample.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { FluentProvider, webLightTheme } from '@fluentui/react-components';
+import { FluentProvider, webLightTheme, webDarkTheme } from '@fluentui/react-components';
 import { Stack } from '@fluentui/react';
 import {
   FullArticle,
@@ -14,21 +14,31 @@ const sampleArticle: Article = {
   shortDescription: 'Short description of the sample article for demonstration purposes.',
   author: 'Jane Doe',
   dateAdded: '2024-06-01',
-  picture: 'https://via.placeholder.com/800x400',
+  picture: 'http://placehold.co/800x400',
   content:
     'Full content of the article goes here. It can span multiple paragraphs and contains detailed information.',
 };
 
 export default function ArticleExample() {
   return (
-    <FluentProvider theme={webLightTheme}>
-      <Stack tokens={{ childrenGap: 16 }}>
-        <FullArticle article={sampleArticle} />
-        <BigCardArticle article={sampleArticle} />
-        <SmallCardArticle article={sampleArticle} />
-        <NoImageCardArticle article={sampleArticle} />
-      </Stack>
-    </FluentProvider>
+    <Stack tokens={{ childrenGap: 32 }}>
+      <FluentProvider theme={webLightTheme}>
+        <Stack tokens={{ childrenGap: 16 }}>
+          <FullArticle article={sampleArticle} />
+          <BigCardArticle article={sampleArticle} />
+          <SmallCardArticle article={sampleArticle} />
+          <NoImageCardArticle article={sampleArticle} />
+        </Stack>
+      </FluentProvider>
+      <FluentProvider theme={webDarkTheme}>
+        <Stack tokens={{ childrenGap: 16 }}>
+          <FullArticle article={sampleArticle} />
+          <BigCardArticle article={sampleArticle} />
+          <SmallCardArticle article={sampleArticle} />
+          <NoImageCardArticle article={sampleArticle} />
+        </Stack>
+      </FluentProvider>
+    </Stack>
   );
 }
 
